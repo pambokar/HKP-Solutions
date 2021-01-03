632e78b96dbfd802b272a3999f5b7390db950672
pambokar <68516639+pambokar@users.noreply.github.com>
2021-01-03 18:12:39 -0500

    Add files via upload

---
+++ Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Pranesh Ambokar on 1/3/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
